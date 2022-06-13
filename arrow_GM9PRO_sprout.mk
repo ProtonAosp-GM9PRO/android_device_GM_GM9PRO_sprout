7f0ad812257e55a6ff4862f73f10e63fd2023590
Ömer Aşıklı <104590063+zangyapro@users.noreply.github.com>
2022-06-13 12:35:18 +0300

    Update arrow_GM9PRO_sprout.mk

--- arrow_GM9PRO_sprout.mk
+++ arrow_GM9PRO_sprout.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -17,13 +17,13 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := GM
 PRODUCT_DEVICE := GM9PRO_sprout
 PRODUCT_MANUFACTURER := General Mobile
-PRODUCT_NAME := arrow_GM9PRO_sprout
+PRODUCT_NAME := lineage_GM9PRO_sprout
 PRODUCT_MODEL := GM 9 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-gm
 TARGET_VENDOR := GM
 TARGET_VENDOR_PRODUCT_NAME := GM9PRO_sprout
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="gm9pro-user 9 PKQ1.180904.001 315 release-keys"
+	PRIVATE_BUILD_DESC="raven-user 12 SP3A.220605.009.B1 8650216 release-keys"
 
-BUILD_FINGERPRINT := essential/mata/mata:8.1.0/OPM1.180104.092/224:user/release-keys
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
